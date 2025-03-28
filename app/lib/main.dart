eb6f8c2e5fab2eb2d169d9db0a95349f791be1d1
WeirdOrange <ivanlai2005@gmail.com>
2025-03-28 12:22:35 +0800

    Camera features preparations

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,15 +4,20 @@ import 'package:alt_f4/pages/food_page.dart';
 import 'package:alt_f4/pages/profile_page.dart';
 import 'package:alt_f4/pages/signup_page.dart';
 import 'package:alt_f4/pages/settings_page.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras,));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<CameraDescription> cameras;
+  const MyApp({super.key, required this.cameras});
 
   // This widget is the root of your application.
   @override
